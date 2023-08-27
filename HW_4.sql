273b94527ea38a420e0311744496863b1c7845ff
ArtemDK <110335982+ArtemDK@users.noreply.github.com>
2023-08-27 17:54:52 +0700

    Update HW_4.sql
    add Task3

--- HW_4.sql
+++ HW_4.sql
@@ -9,3 +9,8 @@ SELECT profiles.gender, SUM(likes.media_id) AS total_likes
 FROM likes
 JOIN profiles ON likes.user_id = profiles.user_id
 GROUP BY profiles.gender;
+
+/*Вывести всех пользователей, которые не отправляли сообщения. */
+SELECT firstname, lastname
+FROM users
+WHERE id NOT IN (SELECT DISTINCT from_user_id FROM messages);
